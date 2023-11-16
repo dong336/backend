a2cadf3ee689c547461a3d6daa8a7179f7efa49a
kdw <50583568+dong336@users.noreply.github.com>
2023-11-16 22:45:04 +0900

    queryDSL을 적용하자

--- src/main/java/com/api/backend/config/JpaConfig.java
+++ src/main/java/com/api/backend/config/JpaConfig.java
@@ -1,6 +1,11 @@
 package com.api.backend.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -10,16 +15,22 @@ import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Arrays;
-import javax.sql.DataSource;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class JpaConfig {
 
-    @Autowired
-    private ApplicationContext applicationContext;
+    private final ApplicationContext applicationContext;
+    private final EntityManager entityManager;
+    
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    } 
 
     @Bean
     public ResourcePatternResolver resourcePatternResolver() {
